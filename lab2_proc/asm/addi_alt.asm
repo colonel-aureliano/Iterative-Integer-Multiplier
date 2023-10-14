a5a3d1b8daeb6e598cbb2035f78035591fcec11e
jx353 <jx353@cornell.edu>
2023-10-14 12:08:55 -0400

    addi procalt & asm

---
+++ lab2_proc/asm/addi_alt.asm
@@ -0,0 +1,27 @@
+csrr x1, mngr2proc < 5
+nop
+nop
+nop
+nop
+nop
+nop
+nop
+nop
+addi x3, x1, 1
+nop
+nop
+nop
+nop
+nop
+nop
+nop
+nop
+csrw proc2mngr, x3 > 6
+nop
+nop
+nop
+nop
+nop
+nop
+nop
+nop
