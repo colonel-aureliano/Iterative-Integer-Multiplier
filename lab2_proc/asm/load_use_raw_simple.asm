09775d52801834b86b1bb9f6baa34b33fc4009b4
rz367 <yanny20011102@gmail.com>
2024-04-28 03:06:30 -0400

    Delete jiahan's lab 2 and lab 3 files

--- lab2_proc/asm/load_use_raw_simple.asm
+++
@@ -1,5 +0,0 @@
-csrr x1, mngr2proc < 17
-csrr x2, mngr2proc < 8192
-lw x1, 0(x2)
-addi x3, x1, 1
-csrw proc2mngr, x3 > 1
--- lab2_proc/asm/source_sink.asm
+++
@@ -1,2 +0,0 @@
-csrr x1, mngr2proc   < 0xDEADBEEF
-csrw proc2mngr, x1   > 0xDEADBEEF
